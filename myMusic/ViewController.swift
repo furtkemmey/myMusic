172907852a432c5d34ff3500f76b093443f6ed8a
desktop <desktop@desktop.com>
2018-02-06 15:45:14 +0800

    create a notification @obj func #selector

--- myMusic/ViewController.swift
+++ myMusic/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var audioPlayer: AVAudioPlayer!
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var playNPause: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var playTimeInfo: UILabel!
+    @IBOutlet weak var playLeftTimeInfo: UILabel!
+    
+    // MARK: - IBAction
+    @IBAction func playNPauseAction(_ sender: UIButton) {
     }
 
+    @IBAction func stopPlay(_ sender: UIButton) {
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+    }
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //create a notification
+        let notificationCenterDefault = NotificationCenter.default
+        // add observer
+        notificationCenterDefault.addObserver(self, selector: #selector(audioInterrupted(_:)), name: .AVAudioSessionInterruption, object: nil)
+    }
+
+    // add "@objc" by "#selector"
+    @objc func audioInterrupted(_ notification: Notification) {
+
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
